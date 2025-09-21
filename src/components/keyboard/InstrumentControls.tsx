d45e54e620b69c9263f130af02e7261f197f71f7
pipkim2009 <e4ellis09@gmail.com>
2025-09-21 15:26:22 +0100

    layout improvements

--- src/components/keyboard/InstrumentControls.tsx
+++ src/components/keyboard/InstrumentControls.tsx
@@ -534,25 +534,6 @@ const InstrumentControls: React.FC<InstrumentControlsProps> = ({
         </div>
       )}
 
-      {/* Deselect All button - separate row */}
-      {hasSelectedNotes && (
-        <div className="control-group deselect-row">
-          <button
-            onClick={() => {
-              clearSelection()
-              handleClearScale()
-              handleClearKeyboardScale()
-            }}
-            className="control-button delete-selection"
-            title="Clear selected notes and scales"
-          >
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-            </svg>
-            Deselect All
-          </button>
-        </div>
-      )}
 
       {/* Play, Generate Melody, BPM, Notes - Combined row */}
       <div className="control-group bpm-notes-melody-row">
